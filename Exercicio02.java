f4d804cd5492a857ae37a93d11be0f5c77b3d7a6
takamuio <68620881+takamuio@users.noreply.github.com>
2021-03-18 14:45:59 -0300

    Update Exercicio02.java

--- Exercicio02.java
+++ Exercicio02.java
@@ -1,31 +1,27 @@
 // Leia um vetor de 20 posições. 
 // Contar e escrever quantos valores pares e ímpares ele possui. Imprimir na tela
+
 import java.util.ArrayList;
 
 public class Exercicio02 {
 
-   
     public static void main(String[] args) {
-        ArrayList <Integer> numeros = new ArrayList(); 
-        
+        ArrayList<Integer> numeros = new ArrayList();
+
         int contPar = 0, contImpar = 0;
-        
-        for(int i = 0; i < 20; i++){            
+
+        for (int i = 0; i < 19; i++) {
             numeros.add(i + 1);
-            
-            if(numeros.get(i) % 2 == 0){
+
+            if (numeros.get(i) % 2 == 0) {
                 contPar++;
             }
-            
-            if(numeros.get(i) % 2 == 1){
-                contImpar++;
-            }
-            
+            contImpar = numeros.size() - contPar;
         }
-        
+
         System.out.println("Os numeros digitador foram: " + numeros);
         System.out.println("Foram digitados " + contPar + " números pares.");
         System.out.println("E " + contImpar + " números ímpares.");
     }
-    
+
 }
